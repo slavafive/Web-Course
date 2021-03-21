7c6bb9f30f9a3f21e94d6db8f60553e67ff3c414
Vyacheslav Efimov <efimov@element-soft.com>
2021-03-21 12:16:30 +0300

    addCity function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-function addItem() {
-    // var city = document.createElement("li");
-    // city.setAttribute('class', 'city-item');
-    // document.getElementById("city-list").appendChild(city);
+apiKey = 'c1fba4b3fbec3b5f92b79c28980e8024';
 
-    /*
-            <h3>Paris</h3>
-            <p class="temperature-city-information">18&#176;C</p>
-            <img src="images/cloudy.png" class="img-city-weather">
-            <button type="submit" class="round">x</button>
-     */
+localStorage.clear();
+
+function refresh() {
+    let cities = []
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        cities.push(key);
+    }
+    localStorage.clear();
+    for (let i = 0; i < cities.length; i++) {
+        localStorage.setItem(cities[i], 'true');
+        addCity(cities[i]);
+    }
+}
+
+function enterCity() {
+    let cityName = document.getElementById('add-new-city').value;
+
+    if (localStorage.getItem(cityName) != null) {
+        return;
+    }
+
+    addCity(cityName);
+}
+
+function addCity(cityName) {
 
     let h3 = document.createElement('h3');
-    let cityName = document.getElementById('add-new-city').value;
     h3.textContent = cityName;
 
     let p = document.createElement('p');
@@ -35,8 +51,6 @@ function addItem() {
     div.appendChild(img);
     div.appendChild(button);
 
-
-
     let ulChild = document.createElement('ul');
     ulChild.classList.add('city-information-list');
 
@@ -46,6 +60,15 @@ function addItem() {
         'Pressure': '1013 hpa',
         'Humidity': '52 %',
         'Coordinates': '[59.88, 30.42]'
+    }
+
+    // let weatherConditions = getWeather(cityName);
+    try {
+        let a = getWeather(cityName);
+        console.log(weatherConditions);
+    } catch(error) {
+        console.log(error);
+        return;
     }
 
     for (let key in weatherConditions) {
@@ -71,4 +94,27 @@ function addItem() {
 
     let ul = document.getElementById('city-list');
     ul.appendChild(li);
+
+    localStorage.setItem(cityName, 'true');
+}
+
+function removeCity() {
+
+}
+
+// document.getElementById('refresh').addEventListener('click', getWeather);
+
+function getWeather(city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+        .then(response => response.json())
+        .then(data => {
+            let dict = {
+                'latitude': data['coord']['lat'],
+                'longitude': data['coord']['lon'],
+                'windSpeed': data['wind']['speed'],
+                'description': data['weather'][0]['description']
+            };
+            return 5;
+        })
+        .catch(err => alert('Wrong city name!'));
 }
