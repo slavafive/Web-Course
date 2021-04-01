b2aec7040a0279f983418ee5de682b5eb72cbc4b
Vyacheslav Efimov <efimov@element-soft.com>
2021-04-01 10:09:24 +0300

    Fix duplicate cities bug

--- nodejs/file.js
+++ nodejs/file.js
@@ -1,1 +1,24 @@
-console.log(666)
+const http = require("http")
+const express = require("express")
+const app = express()
+
+// http.createServer(function (request, response) {
+//
+//     response.write("Hi server")
+//
+//     response.end()
+// }).listen(3000, "127.0.0.1", function () {
+//     console.log("hi")
+// })
+
+// server.listen(3000, () => {
+//     console.log('Server was started')
+// })
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home!')
+})
+
+// LISTENING
+app.listen(3000)
